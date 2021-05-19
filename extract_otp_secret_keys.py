516c2b95320b10fdbfd4fca49568f91879e21f94
Owais Shaikh <71916237+4f77616973@users.noreply.github.com>
2021-05-19 17:28:14 +0530

    Modded script to only output json data

--- extract_otp_secret_keys.py
+++ extract_otp_secret_keys.py
@@ -51,14 +51,8 @@ from re import sub, compile as rcompile
 import generated_python.google_auth_pb2
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('--verbose', '-v', help='verbose output', action='store_true')
-arg_parser.add_argument('--saveqr', '-s', help='save QR code(s) as images to the "qr" subfolder', action='store_true')
-arg_parser.add_argument('--printqr', '-p', help='print QR code(s) as text to the terminal', action='store_true')
-arg_parser.add_argument('infile', help='file or - for stdin (default: -) with "otpauth-migration://..." URLs separated by newlines, lines starting with # are ignored')
+arg_parser.add_argument('otpauthurl', help='string with "otpauth-migration://..." URLs separated by newlines, lines starting with # are ignored')
 args = arg_parser.parse_args()
-
-if args.saveqr or args.printqr: from qrcode import QRCode
-verbose = args.verbose
 
 # https://stackoverflow.com/questions/40226049/find-enums-listed-in-python-descriptor-for-protobuf
 def get_enum_name_by_number(parent, field_name):
@@ -68,55 +62,40 @@ def get_enum_name_by_number(parent, field_name):
 def convert_secret_from_bytes_to_base32_str(bytes):
     return str(base64.b32encode(otp.secret), 'utf-8').replace('=', '')
 
-def save_qr(data, name):
-    qr = QRCode()
-    qr.add_data(data)
-    img = qr.make_image(fill_color='black', back_color='white')
-    if verbose: print('Saving to {}'.format(name))
-    img.save(name)
-
-def print_qr(data):
-    qr = QRCode()
-    qr.add_data(data)
-    qr.print_tty()
-
 i = j = 0
-for line in (line.strip() for line in fileinput.input(args.infile)):
-    if verbose: print(line)
-    if line.startswith('#') or line == '': continue
-    if not line.startswith('otpauth-migration://'): print('\nWARN: line is not a otpauth-migration:// URL\ninput file: {}\nline "{}"\nProbably a wrong file was given'.format(args.infile, line))
-    parsed_url = urlparse(line)
-    params = parse_qs(parsed_url.query)
-    if not 'data' in params:
-        print('\nERROR: no data query parameter in input URL\ninput file: {}\nline "{}"\nProbably a wrong file was given'.format(args.infile, line))
-        sys.exit(1)
-    data_encoded = params['data'][0]
-    data = base64.b64decode(data_encoded)
-    payload = generated_python.google_auth_pb2.MigrationPayload()
-    payload.ParseFromString(data)
-    i += 1
-    if verbose: print('\n{}. Payload Line'.format(i), payload, sep='\n')
 
-    # pylint: disable=no-member
-    for otp in payload.otp_parameters:
-        j += 1
-        if verbose: print('\n{}. Secret Key'.format(j))
-        else: print()
-        print('Name:   {}'.format(otp.name))
-        secret = convert_secret_from_bytes_to_base32_str(otp.secret)
-        print('Secret: {}'.format(secret))
-        if otp.issuer: print('Issuer: {}'.format(otp.issuer))
-        print('Type:   {}'.format(get_enum_name_by_number(otp, 'type')))
-        url_params = { 'secret': secret }
-        if otp.type == 1: url_params['counter'] = otp.counter
-        if otp.issuer: url_params['issuer'] = otp.issuer
-        otp_url = 'otpauth://{}/{}?'.format('totp' if otp.type == 2 else 'hotp', quote(otp.name)) + urlencode(url_params)
-        if verbose: print(otp_url)
-        if args.printqr:
-            print_qr(otp_url)
-        if args.saveqr:
-            if not(path.exists('qr')): mkdir('qr')
-            pattern = rcompile(r'[\W_]+')
-            file_otp_name = pattern.sub('', otp.name)
-            file_otp_issuer = pattern.sub('', otp.issuer)
-            save_qr(otp_url, 'qr/{}-{}{}.png'.format(j, file_otp_name, '-' + file_otp_issuer if file_otp_issuer else ''))
+line = args.otpauthurl
+
+if line.startswith('#') or line == '': print("no data in string passed")
+if not line.startswith('otpauth-migration://'): print('\nWARN: line is not a otpauth-migration:// URL\ninput file: {}\nline "{}"\nProbably a wrong file was given'.format(args.infile, line))
+parsed_url = urlparse(line)
+params = parse_qs(parsed_url.query)
+if not 'data' in params:
+    print('\nERROR: no data query parameter in input URL\ninput file: {}\nline "{}"\nProbably a wrong file was given'.format(args.infile, line))
+    sys.exit(1)
+data_encoded = params['data'][0]
+data = base64.b64decode(data_encoded)
+payload = generated_python.google_auth_pb2.MigrationPayload()
+payload.ParseFromString(data)
+i += 1
+# pylint: disable=no-member
+json = ""
+json+=("{")
+
+for otp in payload.otp_parameters:
+    j += 1
+    secret = convert_secret_from_bytes_to_base32_str(otp.secret)
+    url_params = { 'secret': secret }
+    if otp.type == 1: url_params['counter'] = otp.counter
+    if otp.issuer: url_params['issuer'] = otp.issuer
+    otp_url = 'otpauth://{}/{}?'.format('totp' if otp.type == 2 else 'hotp', quote(otp.name)) + urlencode(url_params)
+
+    if otp.issuer == "": json+=(otp.name+"")
+    json+=(otp.issuer+":")
+    json+=("{")
+    json+=("\"id\""+":"+"\""+secret+"\""+",")
+    json+=("\"username\""+":"+"\""+otp.name+"\""+",")
+    json+=("\"type\""+":"+"\""+str(otp.type)+"\""+",")
+    json+=("},")
+json+=("}")
+print(json)
